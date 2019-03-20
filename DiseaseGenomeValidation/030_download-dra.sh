3d77d3c0c9b3ce6ba12b48ee39292c779216bf16
Hiroyuki Mishima <misshie@hirombp.local>
2019-03-20 17:11:05 +0900

    validation Tomita genome

--- DiseaseGenomeValidation/030_download-dra.sh
+++ DiseaseGenomeValidation/030_download-dra.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 site="ftp://ftp.ddbj.nig.ac.jp"
 path1=ddbj_database/dra/sralite/ByExp/litesra
-path2=DRX/DRX005/DRX005949/DRR006760
-path3=DRR002191.sra
+path2=DRX/DRX001/DRX001619/
+path3=DRR002191/DRR002191.sra
 curl -O ${site}/${path1}/${path2}/${path3}
 ls -lh ${path3}
--- DiseaseGenomeValidation/040_run-sra-fastq-dump.sh
+++ DiseaseGenomeValidation/040_run-sra-fastq-dump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 fdump=sratoolkit.2.9.4-mac64/bin/fastq-dump
-sra=DRR006760.sra
-fq1=DRR006760_1.fastq
-fq2=DRR006760_2.fastq
+sra=DRR002191.sra
+fq1=DRR002191_1.fastq
+fq2=DRR002191_2.fastq
 {fdump} --split-files ${sra}
 gzip ${fq1}
 gzip ${fq2}
