845b201dfd15bdce6090d1cef788f54cd9531b45
Hiroyuki Mishima <misshie@be.to>
2019-06-12 11:59:57 +0900

    use SRAtoolkit v2.9.6-1

--- DiseaseGenomeMain/040_run-sra-fastq-dump.sh
+++ DiseaseGenomeMain/040_run-sra-fastq-dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-fdump=sratoolkit.2.9.4-mac64/bin/fastq-dump
+fdump=sratoolkit.2.9.6-1-mac64/bin/fastq-dump
 sra=DRR006760.sra
 fq1=DRR006760_1.fastq
 fq2=DRR006760_2.fastq
--- DiseaseGenomeValidation/040_run-sra-fastq-dump.sh
+++ DiseaseGenomeValidation/040_run-sra-fastq-dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-fdump=sratoolkit.2.9.4-mac64/bin/fastq-dump
+fdump=sratoolkit.2.9.6-1-mac64/bin/fastq-dump
 sra=DRR002191.sra
 fq1=DRR002191_1.fastq
 fq2=DRR002191_2.fastq
