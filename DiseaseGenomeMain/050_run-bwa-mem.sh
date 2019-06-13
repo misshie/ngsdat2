dcd8cffe8f6e724bd3aa4e3bb0622e8380a6e119
Hiroyuki Mishima <misshie@be.to>
2019-06-13 17:45:52 +0900

    correct main-050

--- DiseaseGenomeMain/050_run-bwa-mem.sh
+++ DiseaseGenomeMain/050_run-bwa-mem.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-bwa=../bwa-0.7.17/bwa
+bwa=bwa-0.7.17/bwa
 id=DRR006760
 fq1=${id}_1.fastq.gz
 fq2=${id}_2.fastq.gz
-ref=hg38.fasta
+ref=RefHg38/hg38.fasta
 rg="@RG\tID:${id}\tSM:${id}\tPL:illumina\tLB:${id}"
 ${bwa} mem \
        -R ${rg} \
