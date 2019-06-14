f1b872ca269a1d28b9d29539d54e3076940955fe
Hiroyuki Mishima <misshie@be.to>
2019-06-14 13:11:33 +0900

    Mail/Validation: correct 080 (undo)

--- DiseaseGenomeMain/080_download-bundles.sh
+++ DiseaseGenomeMain/080_download-bundles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 site=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38
-curl -l -O ${site}/dbsnp_146.hg38.vcf.gz
-curl -l -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
-curl -l -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-curl -l -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+curl -L -O ${site}/dbsnp_146.hg38.vcf.gz
+curl -L -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
+curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
