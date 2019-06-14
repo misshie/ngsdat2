d623ec2f51440544fe7efa4c44554d3a82aa4bfa
Hiroyuki Mishima <misshie@be.to>
2019-06-14 13:19:42 +0900

    Mail/Validation: correct 080 to use the -v option

--- DiseaseGenomeValidation/080_download-bundles.sh
+++ DiseaseGenomeValidation/080_download-bundles.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 site=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38
 
-curl -L -O ${site}/dbsnp_146.hg38.vcf.gz
-curl -L -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
-curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+curl -v -L -O ${site}/dbsnp_146.hg38.vcf.gz
+curl -v -L -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
+curl -v -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+curl -v -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
