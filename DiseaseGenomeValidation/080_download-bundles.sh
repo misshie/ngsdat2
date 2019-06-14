9c7eed1050091b8355c6b1c1271d1ede61034542
Hiroyuki Mishima <misshie@be.to>
2019-06-14 10:35:50 +0900

    Validation: 080 use the -L option for curl

--- DiseaseGenomeValidation/080_download-bundles.sh
+++ DiseaseGenomeValidation/080_download-bundles.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 site=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38
 
-curl -O ${site}/dbsnp_146.hg38.vcf.gz
-curl -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
-curl -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-curl -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+curl -L -O ${site}/dbsnp_146.hg38.vcf.gz
+curl -L -O ${site}/dbsnp_146.hg38.vcf.gz.tbi
+curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+curl -L -O ${site}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
