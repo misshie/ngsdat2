2d71a497ef54a9d51157e446fb5b4a59d3cfdaaf
Hiroyuki Mishima <misshie@be.to>
2019-06-17 07:06:54 +0900

    Validation: update 320/330

--- DiseaseGenomeValidation/320_run-awk-prioritize.sh
+++ DiseaseGenomeValidation/320_run-awk-prioritize.sh
@@ -11,19 +11,19 @@ f5=${id}.${ref}_p03_trancate-het.txt
 f6=${id}.${ref}_p03_nonsyn-hom.txt
 f7=${id}.${ref}_p03_nonsym-het.txt
 
-Scripts/awk010_ToMMo35KJPNv2.awk \
+awk -f Scripts/awk010_ToMMo35KJPNv2.awk \
     ${forig} > ${f1}
-Scripts/awk020_TrancateVariants.awk \
+awk -f Scripts/awk020_TrancateVariants.awk \
     ${f1} > ${f2}
-Scripts/awk030_NonsynonymousVariants.awk \
+awk -f Scripts/awk030_NonsynonymousVariants.awk \
     ${f1} > ${f3}
-Scripts/awk040_Homozygous.awk \
+awk -f Scripts/awk040_Homozygous.awk \
     ${f2} > ${f4}
-Scripts/awk050_Heterozygous.awk \
+awk -f Scripts/awk050_Heterozygous.awk \
     ${f2} > ${f5}
-Scripts/awk040_Homozygous.awk \
+awk -f Scripts/awk040_Homozygous.awk \
     ${f3} > ${f6}
-Scripts/awk050_Heterozygous.awk \
+awk -f Scripts/awk050_Heterozygous.awk \
     ${f3} > ${f7}
 fnames=(${forig} ${f1} ${f2} ${f3} ${f4} ${f5} ${f6} ${f7})
 for fname in ${fnames[*]}; do
--- DiseaseGenomeValidation/330_SRY-coverage.sh
+++ DiseaseGenomeValidation/330_SRY-coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 id=DRR002191
-bam=${id}.sort.dedup.bam
-bed=sry.hg38.bed
-sry=$'chrY\t2786854\t2787699'
+bam=${id}.sort.bam
+sry="chrY:2786855-2787699"
+out=sry.depth.txt
 
-echo ${sry} > ${bed}
-bedtools coverage -a ${bam} -b $bed
+samtools depth  -r ${sry} ${bam} > ${out}
+cat ${out} | awk '{sum+=$3} END {print sum/NR}'
+
