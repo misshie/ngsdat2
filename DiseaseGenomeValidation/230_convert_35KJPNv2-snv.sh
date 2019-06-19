7bfaa7a0ca59a50b3acb844cdc8f315cbacfe255
Hiroyuki Mishima <misshie@be.to>
2019-06-19 16:07:29 +0900

    main/validation: correct 230 and 240

--- DiseaseGenomeValidation/230_convert_35KJPNv2-snv.sh
+++ DiseaseGenomeValidation/230_convert_35KJPNv2-snv.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 d=20181105open
 dl=Downloads
+separator=../Scripts/tommo_separate_alternatives.rb
+
 pushd annovar-hg38
 # AF
 echo "create tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb using autosome.vcf, chrX_PAR3.vcf and chrMT.vcf"
 foreach vcf in ${dl}/tommo-3.5kjpnv2-${d}-af_snvall-{autosome,chrX_PAR3,chrMT}.vcf.gz
   gunzip -c ${vcf} | perl -F"\t" -lane '$F[7] =~ s/.*?(AF=0.\d+|AF=0.\d+,0.\d+|AF=0.\d+,0.\d+,0.\d+|AF=1);.*/$1/; print join("\t", @F[0,1,1,3,4], $F[7])'
 end | grep -v "^#" > tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb.org
-./tommo_separate_alternatives.rb tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb.org | perl -pe 's/AF=//' > tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb
+${separator} tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb.org | perl -pe 's/AF=//' > tommo-3.5kjpnv2-${d}-af_snvall.MAF.genericdb
 echo "complete"
 
 # other INFO
@@ -19,7 +21,7 @@ foreach vcf in ${dl}/{tommo-3.5kjpnv2-${d}-af_snvall-chrX_PAR3.vcf,tommo-3.5kjpn
   gunzip -c ${vcf} | perl -F"\t" -lane '$F[7] =~ s/AC=.*?;AN=\d+;AF=.*?;(.*)/$1/; $F[7] =~ s/(AF_MALE|AF_FEMALE)=.*?;//g; $F[7] =~ s/;ANN=.*//; $F[7] =~ s/AC_FEMALE/ AC_FEMALE/; print join("\t", @F[0,1,1,3,4], $F[7])'
 end | grep -v "^#" >> tommo-3.5kjpnv2-${d}-af_snvall.INFO.genericdb.org
 #
-../Scripts/tommo_separate_alternatives.rb \
+${separator} \
     tommo-3.5kjpnv2-${d}-af_snvall.INFO.genericdb.org \
     > tommo-3.5kjpnv2-${d}-af_snvall.INFO.genericdb
 echo "complete"
