ab1dbdd0703ae7b38fa2ed4e736c0096c1384283
Hiroyuki Mishima <misshie@be.to>
2019-06-21 08:39:58 +0900

    validation: update 330 to compare SRY to ALB

--- DiseaseGenomeValidation/330_SRY-coverage.sh
+++ DiseaseGenomeValidation/330_SRY-coverage.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 id=DRR002191
 bam=${id}.sort.bam
+alb="chr4:73404349-73421412"
 sry="chrY:2786855-2787699"
-out=sry.depth.txt
+outalb=alb.depth.txt
+outsry=sry.depth.txt
 
-samtools depth  -r ${sry} ${bam} > ${out}
-cat ${out} | awk '{sum+=$3} END {print sum/NR}'
+samtools depth  -r ${alb} ${bam} > ${outalb}
+samtools depth  -r ${sry} ${bam} > ${outsry}
+
+echo "mean depth of the ALB gene on chromosome 4:"
+cat ${outalb} | awk '{sum+=$3} END {print sum/NR}'
+echo "mean depth of the SRY gene on chromosome Y:"
+cat ${outsry} | awk '{sum+=$3} END {print sum/NR}'
 
